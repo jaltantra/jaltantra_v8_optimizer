ff35047d032f75791732c3b0c74c6522db623946
bohemian-bong <chephotography1212@gmail.com>
2025-05-07 06:54:09 +0530

    Phase 1 completed

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 ./mvnw clean
 ./mvnw package
 # java -jar target/JaltantraLoopSB-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev  > /dev/null 2>&1
-java -jar target/JaltantraLoopSB-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev
+java -jar target/JaltantraOptimizer-0.0.1-SNAPSHOT.jar --spring.profiles.active=dev
